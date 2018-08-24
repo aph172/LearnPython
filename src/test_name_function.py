2515444e595904c532d8f99801bce31153eb8382
WT <aph172@hotmail.com>
2018-08-24 14:42:24 -0700

    test with middle name

--- src/test_name_function.py
+++ src/test_name_function.py
@@ -5,7 +5,10 @@ class NamesTestCase(unittest.TestCase):
     def test_first_last_name(self):
         formatted_name = get_formatted_name('janis', 'joplin')
         self.assertEqual(formatted_name, 'Janis Joplin')
-        
+    def test_first_last_middle_name(self):
+        formated_name = get_formatted_name('wolfgang', 'mozart', 'amadeus')
+        self.assertEqual(formated_name, 'Wolfgang Amadeus Mozart')
+
 unittest.main()
 
 
