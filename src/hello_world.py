e07a4da329f73c4505551b5f1f8914f8897fb938
WT <aph172@hotmail.com>
2019-11-12 16:34:49 -0800

    add more learing code

--- src/hello_world.py
+++ src/hello_world.py
@@ -16,17 +16,31 @@ motors.append("Ford")
 motors[0]="Chevy"
 
 for motor in motors:
-	print (motor)
+	print (motor + '\n')
 
 
 alien_0 = {'color': 'green', 'points': 5}
 print(alien_0['color'])
 print(alien_0['points'])
 
+#add new key/value
+alien_0['x_position'] = 10
+alien_0['y_position'] = 20
+
 for a,b in alien_0.items():
 	print ('\nkey:' + a)
 	print ('\nvalue:' + str(b))
 
+print (alien_0['color'])
+
+del alien_0['y_position']
+print (alien_0)
+
+for mykey in sorted(alien_0.keys()):
+    print ('\nKey: ' + mykey)
+
+for myvalue in alien_0.values():
+    print ('\nValue: ' + str(myvalue))
 
 unconfirmed_users = ['alice','brian','candance']
 confirmed_users = []
@@ -36,9 +50,29 @@ while unconfirmed_users:
 	confirmed_users.append(current_user)
 print ("\nThe following users have been confirmed:")
 for confirmed_user in confirmed_users:
-	print (confirmed_user.title())
+    if confirmed_user == 'alice':
+        print (confirmed_user.upper())
+    else:
+	    print (confirmed_user.title())
 
 numbers = list(range(1,6))
 print(numbers)
 for number in numbers:
 	print(number)
+
+#tuple
+dimensions = (20, 30)
+print ('print dimension')
+print (dimensions[0])
+print (dimensions [1])
+for dimension in dimensions:
+    print (dimension)
+
+banned_users = ['andrew', 'michael', 'mary']
+print ('already banned users' + str(banned_users))
+new_user = input('please type in a new user name: ')
+if new_user in banned_users:
+   print (new_user + 'is in the banned list')
+else:
+   banned_users.append(new_user)
+print ('new banned user list' + str(banned_users))
