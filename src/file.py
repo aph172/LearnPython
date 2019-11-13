afe74e8422d69cecdcea93e5ff5bc336fa18f405
WT <aph172@hotmail.com>
2019-11-13 14:08:59 -0800

    parse data and save to a file

--- src/file.py
+++ src/file.py
@@ -3,7 +3,24 @@ dirName = os.getcwd()
 print('Current Directory: ' + dirName)
 listOfFiles = os.listdir(dirName)
 print (listOfFiles)
-fileToOpen = dirName + '/src/test.log'
-with open(fileToOpen) as file_object:
-    myContents = file_object.read()
-    print(myContents)
+fileToOpen = dirName + '\\test.log'
+print ('file to open: ' + fileToOpen)
+
+processedData = ''
+
+# Open data file
+try:
+    rawFile = open(fileToOpen)
+    for line in rawFile:
+        myContents = rawFile.readline()
+        if (myContents.find('UpperAndLowerLimits') >= 0) or (myContents.find('LowerLimitsOnly') >= 0) or (myContents.find('UpperLimitsOnly') >= 0) :
+            processedData += myContents
+finally:
+    rawFile.close()
+print(processedData)
+
+#Create a new file with processed data
+newfile = dirName + '\\processed.txt'
+file_object = open(newfile, 'w')
+file_object.write(processedData)
+file_object.close()
